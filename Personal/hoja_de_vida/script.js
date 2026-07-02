a943f8ec443ca7f53125de1fc642b59f7dd89952
Miguel Giraldo <giraldogarzonmiguelangelm@gmail.com>
2026-07-02 11:04:56 -0500

    ACT Hoja de Vida

--- Personal/hoja_de_vida/script.js
+++ Personal/hoja_de_vida/script.js
@@ -2,7 +2,7 @@ function downloadPDF(){
     const element = document.querySelector("#pdf-content")
     //console.log(element);
     const otp = {
-        margin: [20, 5, 55, 5], //[arriba10, izquierda5, abajo15, derecha5] en mm
+        margin: [10, 8, 10, 8], //[arriba10, izquierda5, abajo15, derecha5] en mm
         filename: 'Hoja_de_vida_Miguel_Giraldo.pdf',
         image: { type: 'jpeg', quality: 1 },
         html2canvas:{
@@ -15,6 +15,10 @@ function downloadPDF(){
             unit: 'mm',
             format: 'a4',
             orientation: 'portrait' //Orientación Vertical
+        },
+        pagebreak: {
+            mode: ['css', 'legacy'],
+            avoid: ['.card', 'section', 'header', 'li', 'h4', 'h5', 'h6']
         }
     }
     html2pdf().set(otp).from(element).save();
